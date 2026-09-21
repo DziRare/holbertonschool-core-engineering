2975fe84fbb698c6e3baeebbf41b9b670d3fbc59
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-09-21 14:54:23 +1000

    fix: string format

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print(calculator_1.add(a, b))
-    print(calculator_1.sub(a, b))
-    print(calculator_1.mul(a, b))
-    print(calculator_1.div(a, b))
+    print("{result}".format(result=calculator_1.add(a, b)))
+    print("{result}".format(result=calculator_1.sub(a, b)))
+    print("{result}".format(result=calculator_1.mul(a, b)))
+    print("{result}".format(result=calculator_1.div(a, b)))
