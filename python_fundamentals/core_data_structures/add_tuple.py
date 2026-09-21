90b5b28d5f0e4ebc501dd78c3cd4b0860c8c9d89
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-09-21 17:23:15 +1000

    feat: complete tuple addition

--- python_fundamentals/core_data_structures/add_tuple.py
+++ python_fundamentals/core_data_structures/add_tuple.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    for _ in range(2):
-        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return new_tuple
+    length = min(len(tuple_a), len(tuple_b))
+    tuple_array = []
 
-print(add_tuple((1, 89), (88, 11)))
-#print(add_tuple((1, 89), (1, )))
-# print(add_tuple((1, 89), ()))
-# print(add_tuple((), ()))
+    for i in range(length):
+        tuple_array.append(tuple_a[i] + tuple_b[i])
+
+    if len(tuple_array) < 1:
+        if len(tuple_a) > 1:
+            tuple_array.append(tuple_a[0])
+        elif len(tuple_b) > 1:
+            tuple_array.append(tuple_b[0])
+        else:
+            tuple_array.append(0)
+
+    if len(tuple_array) < 2:
+        if len(tuple_a) > 1:
+            tuple_array.append(tuple_a[1])
+        elif len(tuple_b) > 1:
+            tuple_array.append(tuple_b[1])
+        else:
+            tuple_array.append(0)
+
+    return tuple(tuple_array)
