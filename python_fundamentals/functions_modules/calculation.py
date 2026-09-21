89e5cb122c0e8befd4448949e5ba4fb9cc10300a
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-09-21 15:29:27 +1000

    fix: full sentences

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{result}".format(result=add(a, b)))
-    print("{result}".format(result=sub(a, b)))
-    print("{result}".format(result=mul(a, b)))
-    print("{result}".format(result=div(a, b)))
+    print("{num_1} + {num_2} = {res}".format(num_1=a, num_2=b, res=add(a, b)))
+    print("{num_1} - {num_2} = {res}".format(num_1=a, num_2=b, res=sub(a, b)))
+    print("{num_1} * {num_2} = {res}".format(num_1=a, num_2=b, res=mul(a, b)))
+    print("{num_1} / {num_2} = {res}".format(num_1=a, num_2=b, res=div(a, b)))
