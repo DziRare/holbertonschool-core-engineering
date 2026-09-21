fd6e6935f18afbb01e2211694c8cf7be301b94ee
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-09-21 11:40:21 +1000

    test: positve number

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -6,7 +6,7 @@ number = __import__("random").randint(-10000, 10000)
 #     last_digit = number % -10
 
 number = 98
-last_digit = number % -10
+last_digit = number % 10
 
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
