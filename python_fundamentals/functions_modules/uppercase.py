3699915b374d1be26568490470b4faae1ad78206
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-09-21 13:08:41 +1000

    feat: uppercase.py

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    difference = ord("a") - ord("A")
+    uppercase_string = ""
+
+    for char in str:
+        char_code = ord(char)
+        if char_code >= ord("a") and char_code <= ord("z"):
+            uppercase_char = chr(char_code - difference)
+            uppercase_string += uppercase_char
+        else:
+            uppercase_string += char
+
+    print(uppercase_string)
