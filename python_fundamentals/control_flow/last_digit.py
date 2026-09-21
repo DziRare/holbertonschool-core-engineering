7733787d36e7264110bf422837a35de38019149d
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-09-21 15:00:00 +1000

    test: remove negatives

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 number = __import__("random").randint(-10000, 10000)
-if number >= 0:
-    last_digit = number % 10
-else:
-    last_digit = number % -10
+
+last_digit = number % 10
 
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
